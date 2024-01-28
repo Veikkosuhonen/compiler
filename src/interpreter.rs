bf2e8348607d923ab7424c0a9f9f53fafb8eed10
Veikkosuhonen <vesuvesuh@gmail.com>
2024-01-28 22:25:11 +0200

    Symtables

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 use crate::parser::Expression;
 use crate::tokenizer::Op;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Value {
     Integer(i32),
     Boolean(bool),
@@ -29,9 +31,9 @@ impl From<Value> for i32 {
 }
 
 impl Op {
-    fn eval_binary_integer(&self, ival1: i32, right_expr: Expression) -> Value {
+    fn eval_binary_integer(&self, ival1: i32, right_expr: Expression, sym_table: &mut Box<SymTable>) -> Value {
         // Integer ops have no short circuiting so just eval right here
-        let ival2: i32 = interpret(right_expr).try_into().expect("Not Value::Integer");
+        let ival2: i32 = interpret_sym_table(right_expr, sym_table).try_into().expect("Not Value::Integer");
 
         match self {
             Op::Add =>       Value::Integer(ival1 + ival2),
@@ -50,12 +52,12 @@ impl Op {
         }
     }
 
-    fn eval_binary_boolean(&self, bval1: bool, right_expr: Expression) -> Value {
+    fn eval_binary_boolean(&self, bval1: bool, right_expr: Expression, sym_table: &mut Box<SymTable>) -> Value {
         match self {
-            Op::Equals =>    Value::Boolean(bval1 == interpret(right_expr).try_into().expect("Not Value::Boolean")),
-            Op::NotEquals => Value::Boolean(bval1 != interpret(right_expr).try_into().expect("Not Value::Boolean")),
-            Op::And =>       Value::Boolean(bval1 && interpret(right_expr).try_into().expect("Not Value::Boolean")),
-            Op::Or =>        Value::Boolean(bval1 || interpret(right_expr).try_into().expect("Not Value::Boolean")),
+            Op::Equals =>    Value::Boolean(bval1 == interpret_sym_table(right_expr, sym_table).try_into().expect("Not Value::Boolean")),
+            Op::NotEquals => Value::Boolean(bval1 != interpret_sym_table(right_expr, sym_table).try_into().expect("Not Value::Boolean")),
+            Op::And =>       Value::Boolean(bval1 && interpret_sym_table(right_expr, sym_table).try_into().expect("Not Value::Boolean")),
+            Op::Or =>        Value::Boolean(bval1 || interpret_sym_table(right_expr, sym_table).try_into().expect("Not Value::Boolean")),
             _ => panic!("Invalid boolean binary operator {:?}", &self)
         }
     }
@@ -75,7 +77,31 @@ impl Op {
     }
 }
 
-pub fn interpret(node: Expression) -> Value {
+struct SymTable<'a> {
+    symbols: HashMap<String,Value>,
+    parent: Option<&'a Box<SymTable<'a>>>,
+}
+
+impl SymTable<'_> {
+    fn new<'a>(parent: Option<&'a Box<SymTable<'a>>>) -> Box<SymTable<'a>> {
+        Box::new(SymTable {
+            symbols: HashMap::new(),
+            parent,
+        })
+    }
+
+    fn get(&self, k: &String) -> Value {
+        if let Some(val) = self.symbols.get(k) {
+            val.clone()
+        } else if let Some(parent) = self.parent {
+            parent.get(k)
+        } else {
+            panic!("Access to undefined symbol '{}'", k)
+        }
+    }
+}
+
+fn interpret_sym_table(node: Expression, sym_table: &mut Box<SymTable>) -> Value {
     match node {
         Expression::IntegerLiteral { value } => {
             Value::Integer(value)
@@ -84,16 +110,15 @@ pub fn interpret(node: Expression) -> Value {
             Value::Boolean(value)
         }
         Expression::BinaryExpression { left, operator, right } => {
-            let left_result = interpret(*left);
-
+            let left_result = interpret_sym_table(*left, sym_table);
             match left_result {
-                Value::Integer(ival1) =>  operator.eval_binary_integer(ival1, *right),
-                Value::Boolean(bval1) => operator.eval_binary_boolean(bval1, *right),
+                Value::Integer(ival1) =>  operator.eval_binary_integer(ival1, *right, sym_table),
+                Value::Boolean(bval1) => operator.eval_binary_boolean(bval1, *right, sym_table),
                 Value::Unit => panic!("Unit used as operand in binary operation"),
             }
         },
         Expression::UnaryExpression { operand, operator } => {
-            let operand_result = interpret(*operand);
+            let operand_result = interpret_sym_table(*operand, sym_table);
             match operand_result {
                 Value::Integer(ival) => operator.eval_unary_integer(ival),
                 Value::Boolean(bval) => operator.eval_unary_boolean(bval),
@@ -101,13 +126,13 @@ pub fn interpret(node: Expression) -> Value {
             }
         }
         Expression::IfExpression { condition, then_branch, else_branch } => {
-            let condition_result = interpret(*condition);
+            let condition_result = interpret_sym_table(*condition, sym_table);
             match condition_result {
                 Value::Boolean(condition_val) => {
                     if condition_val {
-                        interpret(*then_branch)
+                        interpret_sym_table(*then_branch, sym_table)
                     } else if let Some(else_branch) = else_branch {
-                        interpret(*else_branch)
+                        interpret_sym_table(*else_branch, sym_table)
                     } else {
                         Value::Unit
                     }
@@ -116,13 +141,29 @@ pub fn interpret(node: Expression) -> Value {
             }
         },
         Expression::BlockExpression { statements, result } => {
+            let mut inner_sym_table = SymTable::new(Some(sym_table));
             for expr in statements {
-                interpret(*expr);
+                interpret_sym_table(*expr, &mut inner_sym_table);
             }
-            interpret(*result)
-        }
+            interpret_sym_table(*result, &mut inner_sym_table)
+        },
+        Expression::Identifier { value } => sym_table.get(&value),
+        Expression::VariableDeclaration { id, init } => {
+            if let Expression::Identifier { value } = *id {
+                let init_value = interpret_sym_table(*init, sym_table);
+                sym_table.symbols.insert(value, init_value);
+                Value::Unit
+            } else {
+                panic!("Id of a variable declaration must be an identifier");
+            }
+        },
+        Expression::Unit => Value::Unit,
         _ => panic!("Unknown expression {:?}", node)
     }
+}
+
+pub fn interpret(node: Expression) -> Value {
+    interpret_sym_table(node, &mut SymTable::new(None))
 }
 
 #[cfg(test)]
@@ -237,6 +278,60 @@ mod tests {
             assert_eq!(result, 42);
         } else {
             panic!("Wrong!");
+        }
+    }
+
+    #[test]
+    fn variable_declaration() {
+        let res = i("
+            {
+                var minttu = true;
+            }
+        ");
+        assert!(matches!(res, Value::Unit));
+    }
+
+    #[test]
+    fn symbol_reference() {
+        let res = i("
+            {
+                var minttu = 50000;
+                minttu
+            }
+        ");
+        if let Value::Integer(ival) = res {
+            assert_eq!(ival, 50000);
+        } else {
+            panic!("Didnt work!");
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "Access to undefined symbol 'minttuglitch'")]
+    fn undefined_reference() {
+        i("
+            {
+                var minttu = 50000;
+                minttuglitch
+            }
+        ");
+    }
+
+    #[test]
+    fn shadowing() {
+        let res = i("
+            {
+                var minttu = 50000;
+                {
+                    var minttu = 90000;
+                    minttu
+                }
+            }
+        ");
+        if let Value::Integer(ival) = res {
+            assert_eq!(ival, 90000);
+        } else {
+            panic!("Didnt work!");
         }
     }
 }

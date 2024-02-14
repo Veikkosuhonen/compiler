9fce1739d2d2d0392523b0886805a289bff6938e
Veikkosuhonen <vesuvesuh@gmail.com>
2024-02-14 22:57:27 +0200

    Clap clap clap

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,52 @@
-use std::env;
+use clap::{Parser, Subcommand};
+
 use compiler::{asm_generator::generate_asm, e2e::run_tests, ir_generator::{generate_ir, generate_ir_code}, *};
 
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    I { path: String },
+    Ir { path: String },
+    Asm { path: String },
+    E2e,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    let cmd = &args[1];
-    let path = &args.get(2);
+    match &args.command {
+        Commands::I { path } => {
+            let result = interpret_file(&path);
 
-    if let Some(path) = path {
-        match cmd.as_str() {
-            "i" => {
-                let result = interpret_file(path);
-
-                match result {
-                    interpreter::Value::Integer(i) => println!("{}", i),
-                    interpreter::Value::Boolean(b) => println!("{}", b),
-                    interpreter::Value::Function(_) => println!("Function"),
-                    interpreter::Value::Unit => println!("Unit"),
-                }
-            },
-            "ir" => {
-                let typed_ast = typecheck_file(path);
-                let ir = generate_ir(typed_ast);
-                let code = generate_ir_code(ir);
-                println!("{}", code);
-            },
-            "asm" => {
-                let typed_ast = typecheck_file(path);
-                let ir = generate_ir(typed_ast);
-                let asm = generate_asm(ir);
-                println!("{}", asm);
-            },
-            _ => panic!("Unknown command {}", cmd)
+            match result {
+                interpreter::Value::Integer(i) => println!("{}", i),
+                interpreter::Value::Boolean(b) => println!("{}", b),
+                interpreter::Value::Function(_) => println!("Function"),
+                interpreter::Value::Unit => println!("Unit"),
+            }
+        },
+        Commands::Ir { path } => {
+            let typed_ast = typecheck_file(&path);
+            let ir = generate_ir(typed_ast);
+            let code = generate_ir_code(ir);
+            println!("{}", code);
+        },
+        Commands::Asm { path } => {
+            let typed_ast = typecheck_file(&path);
+            let ir = generate_ir(typed_ast);
+            let asm = generate_asm(ir);
+            println!("{}", asm);
+        },
+        Commands::E2e => {
+            run_tests()
         }
-    } else {
-        match cmd.as_str() {
-            "e2e" => {
-                run_tests()
-            },
-            _ => panic!("Unknown command {}", cmd)
-        }
-    }
+    };
 }
